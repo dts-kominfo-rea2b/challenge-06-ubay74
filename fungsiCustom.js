76471af5809f50f72f422d95f010f7b6c5a468a9
ubay74 <105720990+ubay74@users.noreply.github.com>
2022-06-16 16:38:58 +0700

    Update fungsiCustom.js

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,8 +19,31 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
-
+const bacaData = (fnCallback) => {
+  let index1 = "";
+  let index2 = "";
+  let index3 = "";
+  let temp = [];
+  
+  fs.readFile(file1, 'utf-8', (err, data1) => {
+    fs.readFile(file2, 'utf-8', (err, data2) => {
+      fs.readFile(file3, 'utf-8', (err, data3) => {
+        data = [
+          index1 = JSON.parse(data1)['message'].split(" ")[1],
+          index2 = JSON.parse(data2)[0]['message'].split(" ")[1],
+          index3 = JSON.parse(data3)[0]['data']['message'].split(" ")[1]
+          ];
+        
+        temp.push(index1);
+        temp.push(index2);
+        temp.push(index3);
+        
+        fnCallback(err, temp);
+      });
+    });
+  });
+};
+  
 // ! JANGAN DIMODIFIKASI
 module.exports = {
   modifyFile1,
